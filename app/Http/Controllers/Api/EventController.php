d10c87b046741987d8b7651093c1bec3b7c4132e
Mrunal fulzele <mrn26893@gmail.com>
2025-01-05 16:40:08 +0530

    Universal Relation Loading Trait

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\EventResource;
+use App\Http\Traits\CanLoadRelationships;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
@@ -12,37 +13,23 @@ class EventController extends Controller
     /**
      * Display a listing of the resource.
      */
+
+
+     use CanLoadRelationships;
+     private  array $relations = ['user','attendees','attendees.user'];
+
     public function index()
     {
         // $this -> shouldIncludeRelation('user');
-        $query = Event::query();
-        $relations =['user','attendees','attendees.user'];
 
-        foreach ($relations as $relation) {
-            $query = $query->when(
-                $this -> shouldIncludeRelation($relation),
-                fn($q) => $q->with($relation)
-            );
-        }
+        $query = $this ->loadRelationships(Event::query());
 
         return EventResource::collection(
             $query->latest()->paginate());
     }
 
 
-    protected function shouldIncludeRelation( string $relation)
-    {
-        $include = request()->query('include');
 
-        if(!$include){
-
-            return false;
-        }
-
-        $relations = array_map('trim', explode(',', $include));
-
-        return in_array($relation ,$relations);
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -60,7 +47,7 @@ class EventController extends Controller
             'user_id' => 1
         ]);
 
-        return new EventResource( $event);
+        return new EventResource( $this->loadRelationships($event));
 
     }
 
@@ -69,8 +56,8 @@ class EventController extends Controller
      */
     public function show(Event $event)
     {
-        $event->load('user','attendees');
-        return new EventResource( $event);
+
+        return new EventResource( $this->loadRelationships($event));
     }
 
     /**
@@ -87,7 +74,7 @@ class EventController extends Controller
             ])
         );
 
-        return new EventResource( $event);
+        return new EventResource( $this->loadRelationships($event));
     }
 
     /**
---
+++ app/Http/Traits/CanLoadRelationships.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Traits;
+
+use Illuminate\Contracts\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Database\Query\Builder as QueryBuilder;
+
+
+trait CanLoadRelationships
+{
+    public function loadRelationships(
+        Model|QueryBuilder|EloquentBuilder $for,
+        ?array $relations = null
+    ): Model|QueryBuilder|EloquentBuilder
+    {
+        $relations = $relations ?? $this->relations ?? [];
+
+        foreach ($relations as $relation) {
+         $for ->when(
+                $this -> shouldIncludeRelation($relation),
+                fn($q) =>
+                                     $for instanceof Model ?
+                                     $for ->load($relation) : $q->with($relation)
+            );
+        }
+        return $for;
+    }
+
+    protected function shouldIncludeRelation( string $relation)
+    {
+        $include = request()->query('include');
+
+        if(!$include){
+
+            return false;
+        }
+
+        $relations = array_map('trim', explode(',', $include));
+
+        return in_array($relation ,$relations);
+    }
+}
