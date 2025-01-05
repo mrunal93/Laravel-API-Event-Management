f93ad151a977c879c9a08ca4eb48a1535145db4f
Mrunal fulzele <mrn26893@gmail.com>
2025-01-05 16:47:59 +0530

    Loading attendee relations

--- app/Http/Traits/CanLoadRelationships.php
+++ app/Http/Traits/CanLoadRelationships.php
@@ -5,14 +5,14 @@ namespace App\Http\Traits;
 use Illuminate\Contracts\Database\Eloquent\Builder as EloquentBuilder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Database\Query\Builder as QueryBuilder;
-
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 trait CanLoadRelationships
 {
     public function loadRelationships(
-        Model|QueryBuilder|EloquentBuilder $for,
+        Model|QueryBuilder|EloquentBuilder |HasMany $for,
         ?array $relations = null
-    ): Model|QueryBuilder|EloquentBuilder
+    ): Model|QueryBuilder|EloquentBuilder |HasMany
     {
         $relations = $relations ?? $this->relations ?? [];
 
