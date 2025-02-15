52f4d137f7702b6304b6509cd9dfdebaaa3cbac5
Mrunal fulzele <mrn26893@gmail.com>
2025-02-16 03:08:08 +0530

    Reminding About Events - Queues

--- app/Console/Commands/SendEventReminders.php
+++ app/Console/Commands/SendEventReminders.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Notifications\EventReminderNotification;
 use Illuminate\Console\Command;
 use Illuminate\Support\Str;
 
@@ -40,7 +41,9 @@ class SendEventReminders extends Command
         $this->info("Found {$eventCount} {$eventLabel}.");
 
         $events->each(fn ($event)=> $event->attendees->each(
-            fn($attendee)=> $this->info("Notifiying the user {$attendee->user->id}")
+            fn($attendee)=> $attendee->user->notify(
+                new EventReminderNotification( $event)
+            )
         ));
 
         $this->info('Reminder notification sent successfully!!');
---
+++ app/Notifications/EventReminderNotification.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Event;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+
+// -------------------php artisan make:notification EventReminderNotification
+
+//php artisan app:send-event-reminders --------- php artisan queue:work (to run this que)
+class EventReminderNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(
+            public Event $event
+    )
+    {
+
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->line('Reminder: You have an upcomming event!')
+                    ->action('View Event', route('events.show',$this->event->id))
+                    ->line(
+                        "The event {$this->event->name} start at {$this->event->start_time}"
+                    );
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'event_id' => $this->event->id,
+            'event_name' => $this->event->name,
+            'event_start_time' => $this->event->start_time
+        ];
+    }
+}
